ecb590a45a443c3c8466fae1c09549a2fe3f7358
JavierFueyo <157507995+JavierFueyo@users.noreply.github.com>
2025-10-03 10:56:21 +0200

    Verlet hace cosas raras

--- skeleton/Particula.cpp
+++ skeleton/Particula.cpp
@@ -2,17 +2,43 @@
 using namespace physx;
 extern PxMaterial* gMaterial;
 
-Particula::Particula(PxVec3 Pos, PxVec3 Vel, PxVec3 Acel) {
+Particula::Particula(PxVec3 Pos, PxVec3 Vel, PxVec3 Acel, float Damping) {
 	_pos = PxTransform({ Pos.x, Pos.y, Pos.z });
 	_vel = Vel;
 	_acel = Acel;
+	_damping = Damping;
 
 	PxSphereGeometry sphere(1.0f);
 	renderItem = new RenderItem(CreateShape(sphere, gMaterial), &_pos, { 1, 1, 0, 1 });
 }
 
 void
-Particula::integrar(double t) {
-	_vel = _vel + _acel * t;
-	_pos.p = _pos.p + _vel * t;
+Particula::integrarEuler(double t) {
+	_pos.p = _pos.p + t * _vel;
+	_vel = (_vel + _acel * t);
+	_vel = _vel * pow(_damping, t);
+}
+
+void
+Particula::integrarEulerSemiImplicito(double t) {
+	_vel = (_vel + _acel * t);
+	_vel = _vel * pow(_damping, t);
+	_pos.p = _pos.p + t * _vel;
+}
+
+void
+Particula::integrarVerlet(double t) {
+	if (_counter == 0) {
+		_previousPos = _pos.p;
+		_vel = (_vel + _acel * t);
+		_vel = _vel * pow(_damping, t);
+		_pos.p = _pos.p + t * _vel;
+	}
+	else {
+		_sigPos = 2 * _pos.p - _previousPos + (t * t * _acel);
+		//_vel = (_sigPos - _previousPos) / (2 * t);
+		_previousPos = _pos.p;
+		_pos.p = _sigPos;
+	}
+	_counter++;
 }
--- skeleton/Particula.h
+++ skeleton/Particula.h
@@ -2,19 +2,27 @@
 #include "Vector3D.h";
 #include "PxPhysicsAPI.h"
 #include "RenderUtils.hpp"
+#include <cmath>
 using namespace physx;
 
 class Particula
 {
 public:
-	Particula(PxVec3 Pos, PxVec3 Vel, PxVec3 Acel);
+	Particula(PxVec3 Pos, PxVec3 Vel, PxVec3 Acel, float Damping = 1);
 	~Particula();
 
-	void integrar(double t);
+	void integrarEuler(double t);
+	void integrarEulerSemiImplicito(double t);
+	void integrarVerlet(double t);
 
 private:
 	PxVec3 _vel;
 	PxVec3 _acel;
 	physx::PxTransform _pos;
+	float _damping;
+	int _counter = 0;
+	PxVec3 _previousPos;
+	PxVec3 _sigPos;
+
 	RenderItem* renderItem;
 };
